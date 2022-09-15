04bf9bfbf84c4b390428188fbbdc513664cebe63
shehzad <shehzaddiqbal@gmail.com>
2022-09-15 12:50:43 +0500

    second

--- src/Screens/Admin/DashBoard.js
+++ src/Screens/Admin/DashBoard.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import "./dashboard.css"
-import { Button, Modal, TextField, Typography } from '@mui/material';
+import { Button, IconButton, Modal, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import CheckIcon from '@mui/icons-material/Check';
+import { Delete } from '@mui/icons-material';
+import { DataGrid } from '@mui/x-data-grid';
 const DashBoard = () => {
   const [open, setOpen] = useState(false)
 const [Question, setQuestion] = useState("")
@@ -13,6 +15,7 @@ const [Answer3, setAnswer3] = useState("")
 const [Answer4, setAnswer4] = useState("")
 const [displayField, setDisplay] = useState(false)
 const [CorrectAnswer, setCorrectAnswer] = useState("")
+const [Batch, setBatch] = useState("")
 
 
 const [AnswerArray, setAnswerArray] = useState([Answer1,Answer2,Answer3,Answer4])
@@ -43,7 +46,45 @@ const style = {
 };
 
 
-// setAnswerArray([Answer1,Answer2,Answer3,Answer4])
+
+
+
+const columns = [
+  { field: "id", headerName: "Test Id", minWidth: 200, flex: 0.5 },
+    {
+        field: "Category",
+        headerName: "Category",
+        minWidth: 100,
+        flex: 0.3,
+    },
+    {
+        field: "Questions",
+        headerName: "Questions",
+        minWidth: 100,
+        flex: 0.3,
+    },
+  {
+    field: "Batch",
+    headerName: "Batch",
+    minWidth: 150,
+    flex: 0.3,
+},
+    {
+        field: "action",
+        headerName: "Actions",
+        minWidth: 150,
+        flex: 0.3,
+        sortable: false,
+        renderCell: (params) => (
+             <IconButton onClick={()=>alert(params.getValue(params.id, "id"))}>
+             <Delete/>
+             </IconButton>
+        )
+    },
+    ];
+  
+    const rows = [];
+
 
   return (
 
@@ -84,12 +125,16 @@ Add Quiz Question
     ))}
 </select>
 
-<input placeholder='Quiz Question' value={Question} onChange={e => setQuestion(e.target.value)} />
+<input placeholder='Quiz Question' className='QuizQuestion' value={Question} onChange={e => setQuestion(e.target.value)} />
 
-<input placeholder='Quiz Answers1' value={Answer1} onChange={e => setAnswer1(e.target.value)} />
-<input placeholder='Quiz Answers2' value={Answer2} onChange={e => setAnswer2(e.target.value)} />
-<input placeholder='Quiz Answers3' value={Answer3} onChange={e => setAnswer3(e.target.value)} />
-<input placeholder='Quiz Answers4' value={Answer4} onChange={e => setAnswer4(e.target.value)} />
+<div className='answerContainer'>
+<input placeholder='Quiz Options 1' value={Answer1} onChange={e => setAnswer1(e.target.value)} />
+<input placeholder='Quiz Options 2' value={Answer2} onChange={e => setAnswer2(e.target.value)} />
+<input placeholder='Quiz Options 3' value={Answer3} onChange={e => setAnswer3(e.target.value)} />
+<input placeholder='Quiz Options 4' value={Answer4} onChange={e => setAnswer4(e.target.value)} />
+</div>
+
+<input placeholder='Batch Number' className='QuizQuestion' type={"number"} value={Batch} onChange={e => setBatch(e.target.value)} />
 
 <Typography id="modal-modal-title" variant="p" component="p">
 Answer</Typography>
@@ -97,7 +142,7 @@ Answer</Typography>
 
 <Button startIcon={<CheckIcon/>} variant="contained" color="success" onClick={ ()=> { setDisplay(displayField === true ? false : true) }}>True</Button>
 <br/>
-<input placeholder='Correct Answer' value={CorrectAnswer} onChange={e => setCorrectAnswer(e.target.value) }  style={{display:displayField === true ? "block" : "none"}} />
+<input placeholder='Correct Answer'  className='QuizQuestion' value={CorrectAnswer} onChange={e => setCorrectAnswer(e.target.value) }  style={{display:displayField === true ? "block" : "none"}} />
 <br/>
 
 <Button onClick={ ()=> { setOpen(false) }}  variant="contained" color="error">Cancel</Button>
@@ -110,6 +155,20 @@ Answer</Typography>
 
 
 <div className='TableContainer'>
+
+
+<div style={{width:"90%",margin:"auto" }}>
+    <DataGrid
+    rows={rows}
+    columns={columns}
+    pageSize={10}
+    disableSelectionOnClick
+    className="Student Dashboard"
+    autoHeight
+/>
+</div>
+
+
 
 </div>
 
--- src/Screens/SuperAdmin/SuperAdminDashboard.js
+++ src/Screens/SuperAdmin/SuperAdminDashboard.js
@@ -1,15 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch,useSelector} from "react-redux"
 import { AllUser } from '../../Redux/Actions/SuperAdmin'
 import { DataGrid } from '@mui/x-data-grid';
-import { IconButton } from '@mui/material';
+import { Box, Button, IconButton, Modal, Typography } from '@mui/material';
 import { Delete } from '@mui/icons-material';
+import CheckIcon from '@mui/icons-material/Check';
 
 const SuperAdminDashboard = () => {
+    const [open, setOpen] = useState(false)
+    const [Name, setName] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Batch, setBatch] = useState("")
+    const [role, setRole] = useState("")
+    const [Allowed, setAllowed] = useState("")
+    
+    const handleClose = () => setOpen(false)
+    const style = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 400,
+      bgcolor: 'rgb(0, 2, 109)',
+      border: '2px solid transparent',
+      boxShadow: 24,
+      p: 4,
+      color:"white"
+    };
+    
     const dispatch = useDispatch()
     useEffect(() => {
 dispatch(AllUser())
-    }, [])
+    }, [dispatch])
     const {user,error,loading} = useSelector(state => state.Users)
     console.log(user)
 
@@ -45,18 +67,19 @@ dispatch(AllUser())
     const rows = [];
 
 
-    user && user?.Users?.map((item,i)=>(
-rows.push({
-  id:item._id,
-  role:item.role,
-  name:item.name
-})   )
-     ) 
+//     user && user?.Users?.map((item,i)=>(
+// rows.push({
+//   id:item._id,
+//   role:item.role,
+//   name:item.name
+// })   )
+//      ) 
 
   return (
     <div>
 
-<h2 style={{textAlign:"center" ,margin:"5vmax"}}>Welcome to Super Admin DashBoard</h2>
+<h2 style={{textAlign:"center" ,margin:"5vmax"}}> Welcome to Super Admin DashBoard </h2>
+
 <div style={{width:"90%",margin:"auto" }}>
     <DataGrid
     rows={rows}
@@ -67,6 +90,57 @@ rows.push({
     autoHeight
 />
 </div>
+
+<br/>
+<div style={{width:"90%",margin:"auto" }}>
+
+<Button  variant="contained" color="success" onClick={ ()=> { setOpen(true) }}>Save</Button>
+
+</div>
+<div>
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+  className={"modal"}
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Update User 
+    </Typography>
+    
+    <br/>
+
+<input placeholder='Email'  className='QuizQuestion' value={Email} onChange={e => setEmail(e.target.value)} />
+
+<div className='answerContainer'>
+<input placeholder='Name' value={Name} onChange={e => setName(e.target.value)} />
+<input placeholder='Batch' value={Batch} onChange={e => setBatch(e.target.value)} />
+<input placeholder='Allowed to Attempt Quiz' value={Allowed} onChange={e => setAllowed(e.target.value)} />
+<input placeholder='Role' value={role} onChange={e => setRole(e.target.value)} />
+</div>
+
+{/* <input placeholder='Batch Number' className='QuizQuestion' type={"number"} value={Batch} onChange={e => setBatch(e.target.value)} /> */}
+
+
+
+
+{/* <Typography id="modal-modal-title" variant="p" component="p">
+Answer</Typography>  */}
+
+<br/>
+
+
+
+<Button onClick={ ()=> { setOpen(false) }}  variant="contained" color="error">Cancel</Button>
+<Button  variant="contained" color="secondary">Update</Button>
+
+
+  </Box>
+</Modal>
+</div>
+
     </div>
   )
 }
